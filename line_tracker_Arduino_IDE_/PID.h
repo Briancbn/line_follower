10a6e638271df5147c8653fec296d5a11fa82d21
KavNair <37266782+KavNair@users.noreply.github.com>
2018-03-12 20:36:54 +0800

    abs angular + kd value

--- line_tracker_Arduino_IDE_/PID.h
+++ line_tracker_Arduino_IDE_/PID.h
@@ -23,12 +23,12 @@ class PID
     const void load_err(int16_t new_err){
         p_gain = kp * new_err; // calculate p gain
         i_gain += ki * new_err; // integrate i gain
-        d_gain = ki * (new_err - err); // calculate the d gain
+        d_gain = kd * (new_err - err); // calculate the d gain
         err = new_err; // update the error
         int16_t total_gain = p_gain + i_gain + d_gain;
         angular = total_gain;
         if(angular != 0){
-            linear = MAX_ANGULAR_POWER - angular / 2;
+            linear = MAX_ANGULAR_POWER - abs(angular) / 2;
         }
         else
         {
