50e1af0ebfaf4bd280b56ecde3aa87d9bef3519e
Unknown <brian97cbn@gmail.com>
2018-03-14 01:25:44 +0800

    Constrain i gain inside the writable range + fixing some small error + change p i d parameters to float

--- line_tracker_Arduino_IDE_/PID.h
+++ line_tracker_Arduino_IDE_/PID.h
@@ -7,13 +7,13 @@
 class PID
 {
   private:
-    int16_t kp, ki, kd;
-    int16_t p_gain, i_gain, d_gain;
+    float kp, ki, kd;
+    float p_gain, i_gain, d_gain;
     int16_t err;
     int16_t angular, linear;
 
   public:
-    PID(int16_t _kp, int16_t _ki, int16_t _kd)
+    PID(float _kp, float _ki, float _kd)
     : kp(_kp), ki(_ki), kd(_kd), 
       err(0),
       p_gain(0), i_gain(0), d_gain(0)
@@ -23,12 +23,13 @@ class PID
     const void load_err(int16_t new_err){
         p_gain = kp * new_err; // calculate p gain
         i_gain += ki * new_err; // integrate i gain
-        d_gain = ki * (new_err - err); // calculate the d gain
+        d_gain = kd * (new_err - err); // calculate the d gain
         err = new_err; // update the error
+        i_gain = constrain(i_gain, -255, 255);
         int16_t total_gain = p_gain + i_gain + d_gain;
         angular = total_gain;
         if(angular != 0){
-            linear = MAX_ANGULAR_POWER - angular / 2;
+            linear = MAX_ANGULAR_POWER - abs(angular) / 2;
         }
         else
         {
